ad4ae9d927dcfd87781a58667d07a1c7e0bde855
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-06-08 21:08:12 +0300

    It's my change

--- src/pack1/Car1.java
+++ src/pack1/Car1.java
@@ -3,22 +3,19 @@ package pack1;
 public class Car1 {
     String color;
 
-
     public void showColor() {
         System.out.println(color);
     }
 
     public static void main(String[] args) {
-     int a = 5;
-     Car1 car1 = new Car1();
-     car1.color = "red";
+        int a = 5;
+        Car1 car1 = new Car1();
+        car1.color = "red";
 
-     Car1 car2 = new Car1();
-     car2.color = "black";
+        Car1 car2 = new Car1();
+        car2.color = "black";
 
-     System.out.println(car1.color);
-
-
-
+        System.out.println(car1.color);
+        System.out.println("my change");
     }
 }
